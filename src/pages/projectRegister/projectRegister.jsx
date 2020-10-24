719adac145917f322adc4176bfaeaea11504caaf
Dhiego Corrêa <dhicorrea2@gmail.com>
2020-10-24 19:18:37 -0300

    starting the register page

--- src/pages/projectRegister/projectRegister.jsx
+++ src/pages/projectRegister/projectRegister.jsx
@@ -4,8 +4,26 @@ import './projectRegister.css';
 
 import Header from '../../components/header/header';
 
+import iconParty from '../../images/iconParty.png';
+
 export default function ProjectRegister(){
   return(
-    <Header />
+    <React.Fragment>
+      <Header />
+
+      <div className="registerContainer">
+        <div className="postValue">
+          <span>$ 29</span>
+          <div className="textContainer">
+            <p>Enjoy the <strong>promocional</strong> price</p>
+            <img src={iconParty} alt="party icon" />
+          </div>
+        </div>
+
+        <form>
+          <div className="formGroup"></div>
+        </form>
+      </div>
+    </React.Fragment>
   )
 }
